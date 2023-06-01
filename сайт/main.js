84eefa877b259c498f41abdc2e8a338d2ff3bb73
ogyreck <andrej.peredvigin@mail.ru>
2023-06-01 15:18:27 +0300

    форматирование файла
    Добавил комментарии

--- сайт/main.js
+++ сайт/main.js
@@ -1,24 +1,19 @@
 const spaceToPast = document.querySelector('.section-6_api-info');
 const btn = document.querySelector("#button-api");
 const textToHide = spaceToPast.querySelector(".section-4__text")
-console.log(spaceToPast);
-let data = null;
 
+//функция выозва api и обработка данных
 const userAction = async () => {
     const response = await fetch('https://fakestoreapi.com/products');
     const myJson = await response.json();
-    
-    
-    
+
     myJson.forEach((e)=>{
         addDataToSite(e);
-
     })
-    console.log('ок')
-    
-    
+ 
 }
 
+//функция для добавления данных на страницу 
 function addDataToSite(json){
     
     const htmlCode = `<div class="api_card">
@@ -31,12 +26,12 @@ function addDataToSite(json){
     spaceToPast.append(div);
 }
 
-
-
+//отработчик событий с кнопки 
 btn.addEventListener('click',  ()=>{
     textToHide.style.display = "none";
-    setTimeout(userAction,1000)});
+    setTimeout(userAction,1000)
+});
 
-console.log(data);
+
 
 
