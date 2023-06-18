bd587dbc884766747ce49cf4d1abc92430dfd512
ogyreck <andrej.peredvigin@mail.ru>
2023-06-18 12:57:08 +0300

    Добавление новых таблиц
    Добавление новых таблиц

--- Нормализация таблицы.sql
+++ Нормализация таблицы.sql
@@ -1,58 +1,73 @@
-﻿-- Создание таблицы "Покупатель"
+﻿
 CREATE TABLE Buyer (
   BuyerId INT PRIMARY KEY IDENTITY(1,1),
   FirstName NVARCHAR(50),
   MiddleName NVARCHAR(50),
   LastName NVARCHAR(50),
-  Gender CHAR(1),
-  Adress NVARCHAR(100)
+  Gender CHAR(1)
+
 );
 
--- Создание таблицы "Товар"
+
 CREATE TABLE Product (
   ProductId INT PRIMARY KEY IDENTITY(1,1),
-  NameProduct NVARCHAR(50),
-  Price DECIMAL(18,2)
+  NameProduct NVARCHAR(50)
 );
 
--- Создание таблицы "Покупка"
-CREATE TABLE Purchase (
-  PurchaseId INT PRIMARY KEY IDENTITY(1,1),
-  idBuyer INT,
-  idProduct INT,
-  DatePurchase DATE,
-  Quantity INT,
-  FOREIGN KEY (idBuyer) REFERENCES Buyer(BuyerId),
-  FOREIGN KEY (idProduct) REFERENCES Product(ProductId)
+
+CREATE TABLE OrderHeader (
+    OrderHeaderID INT IDENTITY(1, 1) PRIMARY KEY,
+    BuyerId INT,
+    DateBuyer DATE,
+    City NVARCHAR(50),
+    Adress NVARCHAR(100),
+    FOREIGN KEY (BuyerId) REFERENCES Buyer(BuyerId)
 );
 
--- Добавление данных в таблицу "Покупатель"
-INSERT INTO Buyer (FirstName, MiddleName,LastName, Gender, Adress)
-VALUES ('Петр',NULL,'Романов', 'М', 'СПб, Сенатская площадь д.1'),
-       ('Софи́я́', N'Авгу́ста Фредери́ка', 'А́нгальт-Це́рбстская', 'Ж', 'СПб, площадь Островского д.1'),
-       ('Александр', 'Рюрикович', NULL, 'М', 'СПб, пл. Александра Невского д.1');
-       
+CREATE TABLE OrderDetail (
+    OrderDetailID INT IDENTITY(1, 1) PRIMARY KEY,
+    OrderHeaderID INT ,
+    ProductId INT ,
+    Quantity INT ,
+    Price DECIMAL(18,2)
+    Total AS Price * Quantity, 
+    FOREIGN KEY (OrderHeaderID) REFERENCES OrderHeader(OrderHeaderID),
+    FOREIGN KEY (ProductId) REFERENCES Product(ProductId)
+);
 
--- Добавление данных в таблицу "Товар"
-INSERT INTO Product(NameProduct, Price)
-VALUES ('Рама оконная', 3875),
-       ('Платье бальное', 15000),
-       ('Грудки куриные', 180),
-       ('Салат', 52),
-       ('Топор', 500),
-       ('Пила', 450),
-       ('Доски', 4890),
-       ('Брус', 9390),
-       ('Парусина', 182);
+INSERT INTO Buyer (FirstName, MiddleName,LastName, Gender)
+VALUES ('Петр',NULL,'Романов', 'М' ),
+       ('Софи́я́', N'Авгу́ста Фредери́ка', 'А́нгальт-Це́рбстская', 'Ж'),
+       ('Александр', 'Рюрикович', NULL, 'М');
 
--- Добавление данных в таблицу "Покупка"
-INSERT INTO Purchase (idBuyer, idProduct,DatePurchase, Quantity)
-VALUES (1, 1,'1703-05-27', 1),
-       (2, 2,'1762-06-28', 999),
-       (3, 3,'1242-04-05', 5),
-       (3, 4,'1242-04-05', 5),
-       (1, 5,'1704-11-05', 1),
-       (1, 6,'1704-11-05', 1),
-       (1, 7,'1704-11-05', 200),
-       (1, 8,'1704-11-05', 20),
-       (1, 9,'1704-11-05', 100);
+INSERT INTO Product (NameProduct)
+VALUES ('Рама оконная'),
+       ('Платье бальное'),
+       ('Грудки куриные'),
+	   ('Cалат'),
+       ('Топор'),
+       ('Пила'),
+       ('Доски'),
+       ('Брус'),
+       ('Парусина');
+
+INSERT INTO OrderHeader (BuyerId, DateBuyer, City, Adress)
+VALUES (1, '1703-05-27', 'СПб', 'Сенатская площадь д.1'),
+       (2, '1762-06-28', 'СПб', 'площадь Островского д.1'),
+       (3, '1242-04-05', 'СПб', 'пл. Александра Невского д.1'),
+       (1, '1704-11-05', 'СПб', 'Сенатская площадь д.1');
+
+INSERT INTO OrderDetail (OrderHeaderID, ProductId, Quantity, Price)
+INSERT INTO OrderDetail (OrderHeaderID, ProductId, Quantity, Price)
+VALUES (1, 1, '1', 3875),
+       (2, 2, '999', 15000),
+       (3, 3, '5', 180),
+	   (3, 4, '5', 52),
+       (1, 5, '1', 500),
+       (1, 6, '1', 450),
+       (1, 7, '200', 4890),
+       (1, 8, '20', 9390),
+       (1, 9, '100', 182);
+
+
+
